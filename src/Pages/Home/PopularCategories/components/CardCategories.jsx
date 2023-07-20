6906bb86c108807adb0941ec94e2a0e290bb4d18
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-07-20 15:00:15 -0300

    fix

--- src/Pages/Home/PopularCategories/components/CardCategories.jsx
+++ src/Pages/Home/PopularCategories/components/CardCategories.jsx
@@ -15,7 +15,6 @@ export const CardCategories = ({ props }) => {
         transition="background-color 0.3s, color 0.3s"
         _hover={{ bg: '#93a7c5', color: '#fafafa' }} // Estilos en hover para Link y sus elementos hijos
       >
-        {/* <Image src={cardCategoriesImg} alt={cardCategoriesAlt} /> */}
         <Box boxSize="68px">{props.icon()}</Box>
         <Text fontSize="md" textTransform="capitalize" fontWeight="semibold">
           {props.heading}
